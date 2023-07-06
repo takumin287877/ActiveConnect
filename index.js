73684828a2650661089896d6e32cc004b1c6e612
takumin287877 <tazataku928@gmail.com>
2023-07-07 01:41:21 +0900

    デフォルト完成！

--- index.js
+++ index.js
@@ -6,3 +6,30 @@ const urlparams = new URLSearchParams(window.location.search);
 const keyword = urlparams.get("link");
 print(keyword);
 document.write(keyword)
+
+
+// デフォ
+function button2() {
+    links = keyword.slice(keyword.lastIndexOf("@"));
+    print(links)
+    links = links.replace("@","");
+    userid = (keyword.substring(0, keyword.lastIndexOf("@")));
+    print(userid)
+    print(userid + links)
+    url = ("https://" + links +"/"+ userid)
+    print(url)
+    window.location.href = url
+}
+
+// function button2(){
+// links = keyword.slice(keyword.lastIndexOf("@"));
+// print(links)
+// userid = (keyword.substring(0,keyword.lastIndexOf("@")));
+// print(userid)
+// print(userid+links)
+// }
+
+function button1() {
+    print("button");
+}
+
